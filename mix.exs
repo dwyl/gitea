6bd53bd4d7eb007279fb7dd3d8e2df2146e0da1b
SimonLab <simon.labondance@gmail.com>
2022-05-16 05:47:22 +0100

    Downgrade Elixir version
    Use 1.12 instead of 1.13

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Gitea.MixProject do
     [
       app: :gitea,
       version: "0.1.0",
-      elixir: "~> 1.13",
+      elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       description: "Elixir interface for Gitea server",
