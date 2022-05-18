f3558c91ee72f6d2163792baccf9a914cd33f88e
nelsonic <nelson+github@dwyl.com>
2022-05-18 13:27:04 +0100

    save current branch before running local_branch_create/1 #14

--- test/gitea_test.exs
+++ test/gitea_test.exs
@@ -122,15 +122,16 @@ defmodule GiteaTest do
   test "local_branch_create/1 creates a new branch on the localhost" do
     org_name = "myorg"
     repo_name = create_test_git_repo(org_name)
-    # # delete draft branch if exists:
-    # Git.branch(%Git.Repository{path: @cwd}, ~w(--show-current)) |> IO.inspect()
+    # delete draft branch if exists:
+    {:ok, branch_name} = Git.branch(%Git.Repository{path: @cwd}, ~w(--show-current)) |> IO.inspect()
     Git.branch(Gitea.Helpers.local_git_repo(org_name, repo_name), ~w(-D draft)) |> IO.inspect()
 
     {:ok, res} = Gitea.local_branch_create(org_name, repo_name, "draft")
     assert res == "Switched to a new branch 'draft'\n"
 
     # Cleanup!
-    # {:ok, _} = Git.branch(Gitea.Helpers.local_git_repo(org_name, repo_name), ["-D", repo_name])
+    Git.checkout(%Git.Repository{path: @cwd}, [branch_name])
+    Git.branch(Gitea.Helpers.local_git_repo(org_name, repo_name), ~w(-D draft)) |> IO.inspect()
     teardown_local_and_remote(org_name, repo_name)
   end
 
