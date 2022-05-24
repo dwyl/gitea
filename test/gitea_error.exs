706bcdaebe6e1d3c921bd4ba0fa790b88dbdfd8e
SimonLab <simon.labondance@gmail.com>
2022-05-24 14:42:48 +0100

    Add test for Gitea.Error
    Add more test for the Gitea.Error module

--- test/gitea_error.exs
+++ test/gitea_error.exs
@@ -17,4 +17,14 @@ defmodule GiteaErrorTest do
     err = %Gitea.Error{message: "A message to you Rudy", reason: :rudy_error}
     assert Exception.message(err) == "A message to you Rudy, reason: rudy_error"
   end
+
+  test "message function Gitea.Error" do
+    message = Gitea.Error.message(%Gitea.Error{message: "Gitea error", reason: :gitea_down})
+    assert message == "Gitea error, reason: gitea_down"
+  end
+
+  test "exception function Gitea.Error" do
+    err = %Gitea.Error{message: "Gitea error", reason: :gitea_down}
+    assert Gitea.Error.exception(message: "Gitea error", reason: :gitea_down) == err
+  end
 end
